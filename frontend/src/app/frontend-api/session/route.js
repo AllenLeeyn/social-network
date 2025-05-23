f6db0c46eb0645e8f7aadd24515806f1fe5d81ae
kurizma <foriamkuri@gmail.com>
2025-05-23 22:35:54 +0300

    route.js for sessions; await async implemented

--- frontend/src/app/frontend-api/session/route.js
+++ frontend/src/app/frontend-api/session/route.js
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers';
 
 export async function GET() {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     const sessionId = cookieStore.get('session-id')?.value;
     return Response.json({ sessionId });
 }
