fda6ea469976ddc350c2ccdba39d5621124e4aa3
p-rahimi <parisa_rahimy@yahoo.com>
2025-05-30 08:50:58 +0300

    integrate notifications with changes applied in testMain branch

--- frontend/src/components/notifications/NotificationCard.js
+++ frontend/src/components/notifications/NotificationCard.js
@@ -3,25 +3,27 @@
 import React from 'react';
 
 export default function NotificationCard({ notification }) {
+    const acceptRejectActions = ['follow_request', 'group_invite', 'group_request'];
+    const viewActions = ['follow_request_accepted', 'group_event'];
     return (
-        <div className={`notification-card${notification.isRead ? '' : ' unread'}`}>
+        <div className={`notification-card${notification.is_read ? '' : ' unread'}`}>
         <div className="notification-content">
-            {notification.fromUser && (
+            {notification.from_user && (
             <img
-                src={notification.fromUser.avatar}
-                alt={notification.fromUser.name}
+                src={notification.from_user.avatar}
+                alt={notification.from_user.first_name}
                 className="notification-avatar"
             />
             )}
             <div>
             <div className="notification-message">{notification.message}</div>
             <div className="notification-timestamp">
-                {new Date(notification.timestamp).toLocaleString()}
+                {new Date(notification.created_at).toLocaleString()}
             </div>
             <div className="notification-actions">
-                {notification.actions.includes('accept') && <button>Accept</button>}
-                {notification.actions.includes('decline') && <button>Decline</button>}
-                {notification.actions.includes('view') && <button>View</button>}
+                {acceptRejectActions.includes(notification.target_detailed_type) && <button>Accept</button>}
+                {acceptRejectActions.includes(notification.target_detailed_type) && <button>Decline</button>}
+                {viewActions.includes(notification.target_detailed_type) && <button>View</button>}
             </div>
             </div>
         </div>
