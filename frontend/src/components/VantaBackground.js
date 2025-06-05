5aaf67480fa647691c6381ef1a03cffad0a6e74d
ylee <leeyn.shun@gmail.com>
2025-06-05 14:48:58 +0300

    New settings for vanta.

--- frontend/src/components/VantaBackground.js
+++ frontend/src/components/VantaBackground.js
@@ -16,16 +16,16 @@ export default function VantaBackground({ children }) {
         touchControls: false,
         gyroControls: false,
 
-        points: 2.0,              // default is 12.0, lower = fewer dots
-        maxDistance: 30.0,        // default is 20.0, lower = fewer connections
-        spacing: 20.0,    
+        points: 2,
+        maxDistance: 70,
+        spacing: 60,    
 
-        minHeight: 50.0,
-        minWidth: 50.0,
-        scale: 0.25,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        scale: 3,
         scaleMobile: 1.0,
         color: 0x33FFFF,
-        backgroundColor: 0x111111, // optional: set a background color
+        backgroundColor: 0x111111,
         });
     }
     return () => {
